ca9c16476cf12169fe8ebc754f351be8c7584538
mcgvsu <toneyc@mail.gvsu.edu>
2015-03-10 15:23:29 -0400

    Create Tent.java

---
+++ Tent.java
@@ -0,0 +1,29 @@
+package package1;
+
+import java.util.GregorianCalendar;
+
+public class Tent extends Site{
+
+	private static final long serialVersionUID = 1L;
+	/** the number of occupants on the Site */
+	private int numOfOccupants;
+
+	public Tent() {
+		super();
+	}
+
+	public Tent(String nameReserving, GregorianCalendar checkIn,
+			int daysStaying, GregorianCalendar checkOutOn,
+			int siteNumber, int numOfTenters) {
+		super(nameReserving, checkIn, daysStaying, checkOutOn, siteNumber);
+		this.numOfOccupants = numOfTenters;
+	}
+
+	public int getNumOfTenters() {
+		return numOfOccupants;
+	}
+
+	public void setNumOfTenters(int numOfTenters) {
+		this.numOfOccupants = numOfTenters;
+	}
+}
