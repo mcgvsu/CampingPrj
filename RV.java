095dda7a2ac4ecfb0f1767e24b3ae39749a151f8
mcgvsu <toneyc@mail.gvsu.edu>
2015-03-16 21:19:07 -0400

    Update RV.java
    Added JavaDoc

--- RV.java
+++ RV.java
@@ -1,35 +1,71 @@
+/**********************************************************************
+ * Tent class used to construct an RV object by calling upon its parent
+ * class. Also contains methods which are only applicable to tent sites
+ * @author Conner Toney & Michael Kolarik
+ * @version March 2015
+ *********************************************************************/
+
 package package1;
 
 import java.util.GregorianCalendar;
 
-public class RV extends Site{
+public class Tent extends Site {
 
 	private static final long serialVersionUID = 1L;
-	/** the power supplied to the Site */
-	private int power; //30, 40, 50
+	
+	/** Number of occupants on the Site */
+	private int numOfOccupants;
 
-	public RV() {
+	/******************************************************************
+	 * Default tent constructor, immediately calls parent
+	 *****************************************************************/
+	public Tent() {
 		super();
 	}
 
-	public RV(String nameReserving, GregorianCalendar checkIn,
+	/******************************************************************
+	 * Tent constructor which sets all relevant properties for a tent
+	 * Calls parent class with all properties but number of occupants, 
+	 * which is exclusive to a tent site
+	 * @param nameReserving name tied to site at check-in
+	 * @param checkIn calendar object for check-in date
+	 * @param daysStaying desired amount of time to stay checked in
+	 * @param checkOutOn estimated day for checkout
+	 * @param siteNumber the site which will be occupied by this site
+	 * @param numofTenters the number of people staying on this site
+	 *****************************************************************/
+	public Tent(String nameReserving, GregorianCalendar checkIn,
 			int daysStaying, GregorianCalendar checkOutOn,
-			int siteNumber, int power) {
-		super(nameReserving, checkIn, daysStaying, checkOutOn, siteNumber);
-		this.power = power;
+			int siteNumber, int numOfTenters) {
+		super(nameReserving, checkIn, daysStaying, checkOutOn,
+				siteNumber);
+		this.numOfOccupants = numOfTenters;
 	}
 
-	public int getPower() {
-		return power;
+	/******************************************************************
+	 * Gets the number of tenters on this site
+	 * @return numOfOccupants the number of people staying
+	 *****************************************************************/
+	public int getNumOfTenters() {
+		return numOfOccupants;
 	}
 
-	public void setPower(int power) {
-		this.power = power;
+	/******************************************************************
+	 * Sets the number of tenters for this site
+	 * @param numOfTenters the number of occupants
+	 *****************************************************************/
+	public void setNumOfTenters(int numOfTenters) {
+		this.numOfOccupants = numOfTenters;
 	}
 
+	/******************************************************************
+	 * Method which overrides parent method. Calculates the cost of the
+	 * site based upon days staying, number of occupants and base
+	 * tent site cost
+	 * @return cost of staying
+	 *****************************************************************/
 	@Override
 	public double getCost() {
-		return (30 * daysStaying);
-		//TODO more specific formula in regards to power level?
+		return ((3 * daysStaying) * numOfOccupants);
 	}
 }
