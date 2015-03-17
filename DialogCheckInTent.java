5a1b31d5937dc69213cc8ba9bd3b0c267deb72ce
mcgvsu <toneyc@mail.gvsu.edu>
2015-03-16 22:21:35 -0400

    Update DialogCheckInTent.java

--- DialogCheckInTent.java
+++ DialogCheckInTent.java
@@ -1,3 +1,12 @@
+/**********************************************************************
+Dialog form used to gather information in order to keep track of
+an Tent Site.
+
+@author Conner Toney
+@author Mike Kolarik
+@version GVSU Winter 2015
+ *********************************************************************/
+
 package package1;
 
 import java.awt.GridLayout;
@@ -9,96 +18,154 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import javax.swing.*;
 
-public class DialogCheckInTent extends JDialog implements ActionListener {
+public class DialogCheckInTent extends JDialog 
+implements ActionListener {
+
+	/** default serial ID */
 	private static final long serialVersionUID = 1L;
-	private JLabel nameLabel;
-	private JTextField nameText;
-	private JLabel occupiedOnLabel;
-	private JTextField occupiedOnTxt;
-	private JLabel siteNumberLabel;
-	private JTextField siteNumberTxt;
-	private JLabel numOfDaysReservingLabel;
-	private JTextField numOfDaysReservingTxt;
-	private JLabel numOfTentersLabel;
-	private JTextField numOfTentersTxt;
+
+	/** label used to prompt for name */
+	private JLabel nameLbl;
+
+	/** text field used to gather name */
+	private JTextField nameTxt;
+
+	/** used to prompt for check-in date */
+	private JLabel checkedInOnLbl;
+
+	/** used to gather check-in date */
+	private JTextField checkedInOnTxt;
+
+	/** used to prompt for site number */
+	private JLabel siteNumLbl;
+
+	/** used to gather site number */
+	private JTextField siteNumTxt;
+
+	/** used to prompt for amount of reservation days */
+	private JLabel numReservationDaysLbl;
+
+	/** used to gather amount of reservation days */
+	private JTextField numReservationDaysTxt;
+
+	/** used to prompt for tenters amount */
+	private JLabel numTentersLbl;
+
+	/** used to gather amount of tenters */
+	private JTextField numTentersTxt;
+
+	/** used to attempt to set Site to input information */
 	private JButton okButton;
+
+	/** used to cancel out of dialog */
 	private JButton cancelButton;
-	private Tent unit;
+
+	/** generic Tent object used */
+	private Tent tentSite;
+
+	/** used to display form */
 	private JPanel panel;
+
+	/** notifies outside classes of close status */
 	private int closeStatus;
 
-	public DialogCheckInTent(JFrame pOccupy, Tent pUnit) {
-		super(pOccupy, "Tent Check in", true);
+	/******************************************************************
+    Constructor creates the dialog form GUI
+    @param lockedFrame GUI frame that is locked while dialog is open
+    @param inputTent input Tent
+	 *****************************************************************/
+	public DialogCheckInTent(JFrame lockedFrame, Tent inputTent) {
+
+		//locks given frame while dialog is open, sets title
+		super(lockedFrame, "Tent Check in", true);
 		panel = new JPanel();
-		unit = pUnit;
+		tentSite = inputTent;
 		panel.setLayout(new GridLayout(6, 2));
-		nameLabel = new JLabel("Name: ");
-		nameText = new JTextField();
-		occupiedOnLabel = new JLabel("Occupied on (DD/MM/YYYY): ");
-		occupiedOnTxt = new JTextField();
-		siteNumberLabel = new JLabel("Requested site number: ");
-		siteNumberTxt = new JTextField();
-		numOfDaysReservingLabel = new JLabel("Days reserved: ");
-		numOfDaysReservingTxt = new JTextField();
-		numOfTentersLabel = new JLabel("Number of occupants: ");
-		numOfTentersTxt = new JTextField();
+
+		//sets up form content
+		nameLbl = new JLabel("Last Name: ");
+		nameTxt = new JTextField("Doe");
+		checkedInOnLbl = new JLabel("Checked-in (DD/MM/YYYY): ");
+		checkedInOnTxt = new JTextField("03/17/2015");
+		siteNumLbl = new JLabel("Requested site number (1-5): ");
+		siteNumTxt = new JTextField("1");
+		numReservationDaysLbl = new JLabel("Days reserved: ");
+		numReservationDaysTxt = new JTextField("2");
+		numTentersLbl = new JLabel("Number of tenters: ");
+		numTentersTxt = new JTextField("3");
 		okButton = new JButton("OK");
 		okButton.addActionListener(this);
 		cancelButton = new JButton("Cancel");
 		cancelButton.addActionListener(this);
-		panel.add(nameLabel);
-		panel.add(nameText);
-		panel.add(occupiedOnLabel);
-		panel.add(occupiedOnTxt);
-		panel.add(siteNumberLabel);
-		panel.add(siteNumberTxt);
-		panel.add(numOfDaysReservingLabel);
-		panel.add(numOfDaysReservingTxt);
-		panel.add(numOfTentersLabel);
-		panel.add(numOfTentersTxt);
+
+		//adds everything to panel
+		panel.add(nameLbl);
+		panel.add(nameTxt);
+		panel.add(checkedInOnLbl);
+		panel.add(checkedInOnTxt);
+		panel.add(siteNumLbl);
+		panel.add(siteNumTxt);
+		panel.add(numReservationDaysLbl);
+		panel.add(numReservationDaysTxt);
+		panel.add(numTentersLbl);
+		panel.add(numTentersTxt);
 		panel.add(okButton);
 		panel.add(cancelButton);
 		getContentPane().add(panel);
 		pack();
 	}
 
+	/******************************************************************
+    Determines which action to follow based on what buttons were
+    selected
+    @param e the action event
+	 *****************************************************************/
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == okButton) {
+
+			//used to let outside classes that OK has been selected
 			closeStatus = 1;
-			SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+			SimpleDateFormat dateFormat;
+			dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 			Date date;
 			try {
+
 				//parses date, throws exception if parse fails
-				date = dateFormat.parse(occupiedOnTxt.getText());
-				GregorianCalendar occupiedDate = new GregorianCalendar();
-				occupiedDate.setTime(date);
+				date = dateFormat.parse(checkedInOnTxt.getText());
+				GregorianCalendar checkInDate = new GregorianCalendar();
+				checkInDate.setTime(date);
 
 				//parses for name, throws exception if anything
 				//but letters are in string
-				String name = nameText.getText();
+				String name = nameTxt.getText();
 				if (!name.matches("[a-zA-Z]+"))
-					throw new ParseException("Name error - not letters", 1);
+					throw new ParseException("Error - not letters", 1);
 
 				//parses for site#, throws exception if less than 1
-				int siteNum = Integer.parseInt(siteNumberTxt.getText());
+				// or greater than 5
+				int siteNum = Integer.parseInt(siteNumTxt.getText());
 				if (siteNum < 1 || siteNum > 5)
 					throw new ParseException("Site error", 1);
 
-				//parses for reservation day amount, throws exception if less than 1
-				int reservDays = Integer.parseInt(numOfDaysReservingTxt.getText());
+				//parses for reservation day amount, throws exception 
+				//if less than 1
+				int reservDays = Integer.
+						parseInt(numReservationDaysTxt.getText());
 				if (reservDays < 1)
 					throw new ParseException("Reservation error", 1);
 
-				//parses for tenters, throws exception if not 30 40 or 50
-				int numTenters = Integer.parseInt(numOfTentersTxt.getText());
+				//parses for tenters, throws exception if less than 1
+				int numTenters = Integer.
+						parseInt(numTentersTxt.getText());
 				if (numTenters < 1)
 					throw new ParseException("Tenters error", 1);
 
-				unit.setCheckIn(occupiedDate);
-				unit.setNameReserving(name);
-				unit.setSiteNumber(siteNum);
-				unit.setDaysStaying(reservDays);
-				unit.setNumOfTenters(numTenters);
+				//sets parsed information to the input Tent
+				tentSite.setCheckIn(checkInDate);
+				tentSite.setNameReserving(name);
+				tentSite.setSiteNumber(siteNum);
+				tentSite.setDaysStaying(reservDays);
+				tentSite.setNumOfTenters(numTenters);
 				dispose();
 			}
 			catch(ParseException ex1) {
@@ -111,11 +178,17 @@ public class DialogCheckInTent extends JDialog implements ActionListener {
 			}
 		}
 		if (e.getSource() == cancelButton) {
+
+			//used to let outside classes know cancel has been selected
 			closeStatus = 0;
 			dispose();
 		}
 	}
-	
+
+	/******************************************************************
+    Gets the current closing status of the dialog window
+    @return closeStatus
+	 *****************************************************************/
 	public int getCloseStatus() {
 		return closeStatus;
 	}
