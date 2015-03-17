c337912208a725a6cbfff0fa7c8219f6a443359b
mcgvsu <toneyc@mail.gvsu.edu>
2015-03-16 22:22:07 -0400

    Update DialogGetDateStatus.java

--- DialogGetDateStatus.java
+++ DialogGetDateStatus.java
@@ -1,32 +1,69 @@
+/**********************************************************************
+Dialog used to return information the status of a camp site based on
+the given date.
+
+@author Conner Toney
+@author Mike Kolarik
+@version GVSU Winter 2015
+ *********************************************************************/
+
 package package1;
 
 import java.awt.event.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-
 import javax.swing.*;
 
-public class DialogGetDateStatus extends JDialog implements ActionListener {
+public class DialogGetDateStatus extends JDialog 
+implements ActionListener {
+
+	/** default serial ID */
 	private static final long serialVersionUID = 1L;
+
+	/** used to display content */
 	private JPanel panel;
+
+	/** contains text concerning site statuses */
 	private JTextArea statusText;
+
+	/** used to gather input date */
 	private JTextField inputDateTxt;
+
+	/** used to close out of window */
 	private JButton closeButton;
+
+	/** used to attempt to return status information */
 	private JButton okButton;
+
+	/** input date */
 	private Date inputDate;
+
+	/** list of input Sites */
 	private ArrayList<Site> list;
 
-	public DialogGetDateStatus(JFrame pOccupy, ArrayList<Site> listOfSites) {
-		super(pOccupy, "Date Status", true);
+	/******************************************************************
+    Constructor creates the dialog GUI
+    @param lockedFrame GUI frame that is locked while dialog is open
+    @param inputTent input Tent
+	 *****************************************************************/
+	public DialogGetDateStatus(JFrame lockedFrame, 
+			ArrayList<Site> listOfSites) {
+		
+		//locks given frame while dialog is open, sets title
+		super(lockedFrame, "Camp Site Date Status", true);
 		list = listOfSites;
 		panel = new JPanel();
-		statusText = new JTextArea(5, 75);
-		inputDateTxt = new JTextField(10);
+		
+		//sets up dialog content
+		statusText = new JTextArea(5, 65);
+		inputDateTxt = new JTextField("MM/DD/YYYY", 10);
 		okButton = new JButton("OK");
 		okButton.addActionListener(this);
 		closeButton = new JButton("Close");
 		closeButton.addActionListener(this);
+		
+		//adds everything to panel
 		panel.add(statusText);
 		panel.add(inputDateTxt);
 		panel.add(okButton);
@@ -36,13 +73,19 @@ public class DialogGetDateStatus extends JDialog implements ActionListener {
 
 	}
 
+	/******************************************************************
+    Determines which action to follow based on what buttons were
+    selected
+    @param e the action event
+	 *****************************************************************/
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == okButton) {
-			SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+			SimpleDateFormat dateFormat;
+			dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 			try {
 				inputDate = dateFormat.parse(inputDateTxt.getText());
-				GregorianCalendar occupiedDate = new GregorianCalendar();
-				occupiedDate.setTime(inputDate);
+				GregorianCalendar inputCal = new GregorianCalendar();
+				inputCal.setTime(inputDate);
 				getContent();
 			}
 			catch (ParseException ex) {
@@ -56,12 +99,25 @@ public class DialogGetDateStatus extends JDialog implements ActionListener {
 		}
 	}
 
+	/******************************************************************
+    Gets the current closing status of the dialog window
+    @return inputDate
+	 *****************************************************************/
 	public Date getDate() {
 		return inputDate;
 	}
 
+
+	/******************************************************************
+    Gets the content used to determine the status of dates for
+    all input camp sites
+	 *****************************************************************/
 	public void getContent() {
 		String content = "";
+		
+		//iterates through list of sites, retrieving information from
+		//each site and then outputting name, check-in date, site #,
+		//estimated stay, and days remaining
 		for (int i = 0; i < list.size(); i++) {
 
 			//adds name
@@ -71,7 +127,8 @@ public class DialogGetDateStatus extends JDialog implements ActionListener {
 			//adds check in date
 			GregorianCalendar c = list.get(i).getCheckIn();
 			Date date = c.getTime();
-			SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+			SimpleDateFormat dateFormat;
+			dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 			content += ("Checked in: ");
 			content += (dateFormat.format(date));
 			content += ("\t");
@@ -81,20 +138,20 @@ public class DialogGetDateStatus extends JDialog implements ActionListener {
 			content += (list.get(i).getSiteNumber() + "\t");
 
 			//estimated stay
-			content += ("Estimated days: ");
-			content += (list.get(i).getDaysStaying() + "\t");
+			content += ("Estimated stay: ");
+			content += (list.get(i).getDaysStaying() + " days \t");
 
 			//days remaining
 			Calendar advCal = Calendar.getInstance();
 			advCal.setTime(date);
 			advCal.add(Calendar.DATE, list.get(i).getDaysStaying());
 			Date advDate = advCal.getTime();
+			
 			//calculates time difference between dates
 			long dateDiff = advDate.getTime() - inputDate.getTime();
 			int dayDiff = (int) (dateDiff / (24 * 60 * 60 *1000));
-			if (date.getTime() > inputDate.getTime()) {
+			if (date.getTime() > inputDate.getTime()) 
 				content += ("Days Remaining: N/A");
-			}
 			else
 				content += ("Days Remaining:" + dayDiff);
 
